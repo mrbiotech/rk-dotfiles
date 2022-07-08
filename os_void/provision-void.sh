6f05e98ede7722b56cf2359a36617362bc894dd3
Rich Kulesus <richkulesus@gmail.com>
2022-07-08 14:48:46 -0600

    Adding DWM configs for slstatus, dwm-flexipatch. Modified Picom shadows.

--- os_void/provision-void.sh
+++ os_void/provision-void.sh
@@ -9,3 +9,6 @@ sudo xbps-install zsh vim vim-common vim-colorschemes sakura kitty lynx emacs em
 # Graphical software I like
 sudo xbps-install feh rclone-browser fluxbox aseprite gimp inkscape yabar tint2 pekwm jgmenu ImageMagick graphviz xdot python3-graphviz openbox obconf libopenbox obmenu-generator pcmanfm lightdm lxdm lightdm-gtk3-greeter xfdesktop
 
+sudo xbps-remove -yO
+sudo xbps-remove -yo
+sudo vkpurge rm all
