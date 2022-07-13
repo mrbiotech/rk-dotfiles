8b14295742ad2b0f552309a60f5d885ccacf6075
Rich Kulesus <richkulesus@gmail.com>
2022-07-12 20:54:36 -0600

    Now tmux works for me. Powerline. Removed Alacritty from provisioning scripts. I like Kitty better.

--- os_arch/provision-arch.sh
+++ os_arch/provision-arch.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Update all packages
-sudo xbps-install -Su
+sudo apt update && sudo apt upgrade
 
 # CLI Software I like
-sudo xbps-install zsh vim vim-common vim-colorschemes sakura kitty lynx emacs emacs-common rclone nmap exa rofi dunst picom tmux xdgmenumaker alacritty polybar pastel
+sudo apt install -y zsh vim vim-common vim-colorschemes sakura kitty lynx emacs emacs-common rclone nmap exa rofi dunst picom tmux xdgmenumaker polybar pastel
 
 # Graphical software I like
-sudo xbps-install feh rclone-browser fluxbox aseprite gimp inkscape yabar tint2 pekwm jgmenu ImageMagick graphviz xdot python3-graphviz openbox obconf libopenbox obmenu-generator pcmanfm lightdm lxdm lightdm-gtk3-greeter xfdesktop
+sudo apt install -y feh rclone-browser fluxbox aseprite gimp inkscape yabar tint2 pekwm jgmenu ImageMagick graphviz xdot python3-graphviz openbox obconf libopenbox obmenu-generator pcmanfm lightdm lxdm lightdm-gtk3-greeter xfdesktop
 
--- os_void/provision-void.sh
+++ os_void/provision-void.sh
@@ -4,7 +4,7 @@
 sudo xbps-install -Su
 
 # CLI Software I like
-sudo xbps-install zsh vim vim-common vim-colorschemes sakura kitty lynx emacs emacs-common rclone nmap exa rofi dunst picom tmux xdgmenumaker alacritty polybar pastel
+sudo xbps-install zsh vim vim-common vim-colorschemes sakura kitty lynx emacs emacs-common rclone nmap exa rofi dunst picom tmux xdgmenumaker polybar pastel
 
 # Graphical software I like
 sudo xbps-install feh rclone-browser fluxbox aseprite gimp inkscape yabar tint2 pekwm jgmenu ImageMagick graphviz xdot python3-graphviz openbox obconf libopenbox obmenu-generator pcmanfm lightdm lxdm lightdm-gtk3-greeter xfdesktop
