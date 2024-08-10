7c5daec13400901a598863a3c3002818c2d45651
Rich Kulesus <richkulesus@gmail.com>
2024-08-09 22:52:42 -0600

    Minor Void Linux provisioning additions.

--- os_void/provision-void.sh
+++ os_void/provision-void.sh
@@ -4,10 +4,10 @@
 sudo xbps-install -Su
 
 # CLI Software I like
-sudo xbps-install git zsh vim vim-common vim-colorschemes sakura kitty lynx emacs emacs-common rclone nmap eza rofi dunst picom tmux xdgmenumaker polybar pastel peco jq
+sudo xbps-install git zsh vim vim-common vim-colorschemes sakura kitty lynx emacs emacs-common eza rofi dunst picom tmux xdgmenumaker polybar pastel peco jq nerd-fonts python3-setuptools python3-virtualenv virtualenvwrapper eww
 
 # Graphical software I like
-sudo xbps-install feh rclone-browser fluxbox aseprite gimp inkscape yabar tint2 pekwm jgmenu ImageMagick graphviz xdot python3-graphviz openbox obconf libopenbox obmenu-generator pcmanfm lightdm lxdm lightdm-gtk3-greeter xfdesktop thunar
+sudo xbps-install feh fluxbox gimp inkscape yabar icewm qtile tint2 pekwm jgmenu ImageMagick graphviz xdot python3-graphviz openbox obconf libopenbox obmenu-generator pcmanfm lightdm lxdm lightdm-gtk3-greeter xfdesktop thunar
 
 sudo xbps-remove -yO
 sudo xbps-remove -yo
